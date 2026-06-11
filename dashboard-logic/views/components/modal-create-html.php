1d458962aa4dfade502ef912aaa3889b2017fd02
Juan Carlos Avila <juanvs23@gmail.com>
2026-06-11 16:37:31 -0400

    refactor: reemplazar emojis por SVG en títulos de modales

--- dashboard-logic/views/components/modal-create-html.php
+++ dashboard-logic/views/components/modal-create-html.php
@@ -5,7 +5,10 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content bg-dark border-secondary">
             <div class="modal-header border-secondary">
-                <h5 class="modal-title text-light">🌐 Crear Proyecto HTML</h5>
+                <h5 class="modal-title text-light d-flex align-items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="#E44D26"><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/></svg>
+                    Crear Proyecto HTML
+                </h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
             </div>
             <form method="post" action="/?create_project=html">
